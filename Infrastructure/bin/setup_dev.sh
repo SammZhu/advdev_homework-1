7c68d8db8eabb3e309cb2585bffb5b873901dfa3
Carl Durrant <carldurrant@yahoo.co.uk>
2018-08-11 08:13:11 +1000

    First commit

---
+++ Infrastructure/bin/setup_dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Setup Development Project
+if [ "$#" -ne 1 ]; then
+    echo "Usage:"
+    echo "  $0 GUID"
+    exit 1
+fi
+
+GUID=$1
+echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
+
+# Code to set up the parks development project.
+
+# To be Implemented by Student
+
+oc new-project ${GUID}-parks-dev
