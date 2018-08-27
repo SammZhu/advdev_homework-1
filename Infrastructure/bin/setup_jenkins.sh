e7c4efb27ee4372044e9e82f94bb4cfa0130e3cc
Carl Durrant <carldurrant@yahoo.co.uk>
2018-08-27 14:49:25 +1000

    Added all templates and specifically this commit changed mlbparks-config to a single file

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -12,17 +12,31 @@ REPO=$2
 CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
-# Code to set up the Jenkins project to execute the
-# three pipelines.
-# This will need to also build the custom Maven Slave Pod
-# Image to be used in the pipelines.
-# Finally the script needs to create three OpenShift Build
-# Configurations in the Jenkins Project to build the
-# three micro services. Expected name of the build configs:
-# * mlbparks-pipeline
-# * nationalparks-pipeline
-# * parksmap-pipeline
-# The build configurations need to have two environment variables to be passed to the Pipeline:
+# Create Jenkins objects from templates
+oc create -f project.yaml -n ${GUID}-jenkins
+oc create -f serviceaccounts.yaml -n ${GUID}-jenkins
+oc create -f pvc.yaml -n ${GUID}-jenkins
+oc create -f rolebindings.yaml -n ${GUID}-jenkins
+
+# Create the maven slave pod 
+oc new-build --name=maven-slave-pod \
+    --dockerfile="$(< ./Infrastructure/templates/jenkins/Dockerfile)" \
+    -n $GUID-jenkins
+
+# Set latest tag 
+oc tag jenkins-slave-maven-skopeo-centos7 jenkins-slave-maven-skopeo-centos7:latest -n ${GUID}-jenkins
+
+# Wait for Jenkins to deploy and become ready
+while : ; do
+  echo "Checking if Jenkins pod is Ready..."
+  oc get pod -n ${GUID}-jenkins | grep -v "deploy\|build" | grep -q "1/1"
+  [[ "$?" == "1" ]] || break
+  echo "... not quite yet. Sleeping 20 seconds."
+  sleep 20
+done
+
+
+
 # * GUID: the GUID used in all the projects
 # * CLUSTER: the base url of the cluster used (e.g. na39.openshift.opentlc.com)
 
--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -9,23 +9,24 @@ fi
 GUID=$1
 echo "Setting up Nexus in project $GUID-nexus"
 
-# Code to set up the Nexus. It will need to
-# * Create Nexus
-# * Set the right options for the Nexus Deployment Config
-# * Load Nexus with the right repos
-# * Configure Nexus as a docker registry
-# Hint: Make sure to wait until Nexus if fully up and running
-#       before configuring nexus with repositories.
-#       You could use the following code:
-# while : ; do
-#   echo "Checking if Nexus is Ready..."
-#   oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"
-#   [[ "$?" == "1" ]] || break
-#   echo "...no. Sleeping 10 seconds."
-#   sleep 10
-# done
+# Process template 
 
-# Ideally just calls a template
-# oc new-app -f ../templates/nexus.yaml --param .....
 
-# To be Implemented by Student
+
+# Setup template 
+
+
+
+# Wait for Nexus to fully deploy and become ready
+while : ; do
+  echo "Checking if Nexus is Ready..."
+  #oc get pod -n ${GUID}-nexus | grep -v deploy | grep "1/1"
+  curl -i http://$(oc get route nexus3 --template='{{ .spec.host }}' -n ${GUID}-nexus) 2>&1 /dev/null | grep 'HTTP/1.1 200 OK' > /dev/null
+  [[ "$?" == "1" ]] || break
+  echo "... not quite yet. Sleeping 20 seconds."
+  sleep 20
+done
+
+# Setup Nexus Repos
+./Infrastructure/bin/configure_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}' -n ${GUID}-nexus)
+
