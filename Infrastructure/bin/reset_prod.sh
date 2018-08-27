960ccc920dd4e93f22f1f26aa322ff6d5d423b73
Carl Durrant <carldurrant@yahoo.co.uk>
2018-08-27 15:27:36 +1000

    Added parksmap config

--- Infrastructure/bin/reset_prod.sh
+++ Infrastructure/bin/reset_prod.sh
@@ -8,9 +8,9 @@ if [ "$#" -ne 1 ]; then
 fi
 
 GUID=$1
-echo "Resetting Parks Production Environment in project ${GUID}-parks-prod to Green Services"
+echo "Resetting Parks Production Environment in project ${GUID}-parks-prod to Blue Services"
 
-## Reseting MLBParks Production to blue deployment ##
+# Reset MLBParks Production to blue deployment 
 # Delete configmap and re-create with Blue APPNAME
 oc delete configmap mlbparks-config -n ${GUID}-parks-prod --ignore-not-found=true
 oc create configmap mlbparks-config --from-literal=APPNAME='MLB Parks (Blue)' -n ${GUID}-parks-prod
@@ -18,7 +18,7 @@ oc create configmap mlbparks-config --from-literal=APPNAME='MLB Parks (Blue)' -n
 oc delete service mlbparks -n ${GUID}-parks-prod --ignore-not-found=true
 oc expose dc mlbparks-blue --name=mlbparks --port 8080 --labels=type=parksmap-backend,activeApp=mlbparks-blue -n ${GUID}-parks-prod
 
-## Resetting NationalParks Production to blue deployment ##
+# Reset NationalParks Production to blue deployment 
 # Delete configmap and re-create with Blue APPNAME
 oc delete configmap nationalparks-config -n ${GUID}-parks-prod --ignore-not-found=true
 oc create configmap nationalparks-config --from-literal=APPNAME='National Parks (Blue)' -n ${GUID}-parks-prod
@@ -26,7 +26,7 @@ oc create configmap nationalparks-config --from-literal=APPNAME='National Parks 
 oc delete service nationalparks -n ${GUID}-parks-prod --ignore-not-found=true
 oc expose dc nationalparks-blue --name=nationalparks --port 8080 --labels=type=parksmap-backend,activeApp=nationalparks-blue -n ${GUID}-parks-prod
 
-## Resetting ParksMap Production to blue deployment ##
+# Reset ParksMap Production to blue deployment 
 # Delete configmap and re-create with Blue APPNAME
 oc delete configmap parksmap-config -n ${GUID}-parks-prod --ignore-not-found=true
 oc create configmap parksmap-config --from-literal=APPNAME='ParksMap (Blue)' -n ${GUID}-parks-prod
