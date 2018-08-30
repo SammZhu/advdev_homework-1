eda15ca4c9a3fd6744f4d1582e56ec8061f61c21
Carl Durrant <carldurrant@yahoo.co.uk>
2018-08-30 12:19:02 +1000

    Updated nexus

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -14,11 +14,11 @@ echo "Setting up Nexus in project $GUID-nexus"
 oc policy add-role-to-user admin system:serviceaccount:gpte-jenkins:jenkins -n ${GUID}-nexus
 
 # Setup Nexus
-oc new-app sonatype/nexus3:latest
-oc expose svc nexus3
-oc rollout pause dc nexus3
-oc patch dc nexus3 --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
-oc set resources dc nexus3 --limits=memory=2Gi --requests=memory=1Gi
+oc new-app sonatype/nexus3:latest -n ${GUID}-nexus
+oc expose svc nexus3 -n ${GUID}-nexus
+oc rollout pause dc nexus3 -n ${GUID}-nexus
+oc patch dc nexus3 --patch='{ "spec": { "strategy": { "type": "Recreate" }}}' -n ${GUID}-nexus
+oc set resources dc nexus3 --limits=memory=2Gi --requests=memory=1Gi -n ${GUID}-nexus
 
 # Setup pvc
 echo "apiVersion: v1
@@ -30,26 +30,26 @@ spec:
   - ReadWriteOnce
   resources:
     requests:
-      storage: 4Gi" | oc create -f -
+      storage: 4Gi" | oc create -f - -n ${GUID}-nexus
 
 # mount pvc
-oc set volume dc/nexus3 --add --overwrite --name=nexus3-volume-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc
+oc set volume dc/nexus3 --add --overwrite --name=nexus3-volume-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc -n ${GUID}-nexus
 
 # configure probes
-oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok
-oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/
+oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok -n ${GUID}-nexus
+oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/ -n ${GUID}-nexus
 
 # add port for docker
-oc patch dc nexus3 -p '{"spec":{"template":{"spec":{"containers":[{"name":"nexus3","ports":[{"containerPort": 5000,"protocol":"TCP","name":"docker"}]}]}}}}'
+oc patch dc nexus3 -p '{"spec":{"template":{"spec":{"containers":[{"name":"nexus3","ports":[{"containerPort": 5000,"protocol":"TCP","name":"docker"}]}]}}}}' -n ${GUID}-nexus
 
 # continue rollout 
-oc rollout resume dc nexus3
+oc rollout resume dc nexus3 -n ${GUID}-nexus
 
 # expose service port 
-oc expose dc nexus3 --port=5000 --name=nexus-registry
+oc expose dc nexus3 --port=5000 --name=nexus-registry -n ${GUID}-nexus
 
 # expose route port
-oc create route edge nexus-registry --service=nexus-registry --port=5000
+oc create route edge nexus-registry --service=nexus-registry --port=5000 -n ${GUID}-nexus
 
 # Wait for Nexus to fully deploy and become ready
 while : ; do
